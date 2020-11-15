63dbea7b0b0ec2d7cc1c6acc949f547eac5e9ec0
vladislavStep <vlad.stepanenko996@gmail.com>
2020-11-15 12:12:15 +1000

    0_golang.go

---
+++ 0_golang.go
@@ -0,0 +1,78 @@
+package main
+
+import "fmt"
+
+func main() {
+	// fmt.Println("Welcome to the playground!")
+	// fmt.Println("The time is", time.Now())
+	// fmt.Println(len("Hello World"))
+	// fmt.Println("Hello World"[1])
+
+	// x := "Hello, World!\n"
+	// fmt.Println(x)
+	// const y = 123
+	// fmt.Println(y)
+
+	// fmt.Print("Enter a number: ")
+	// var input float64
+	// fmt.Scanf("%f", &input)
+
+	// output := input * 2
+	// for i := 0; i < 100; i++ {
+	// 	if i%3 == 0 && i%5 == 0 {
+	// 		fmt.Println(i, "- FizzBliz")
+	// 	} else if i%3 == 0 {
+	// 		fmt.Println(i, "- Fizz")
+	// 	} else if i%5 == 0 {
+	// 		fmt.Println(i, "- Bliz")
+	// 	}
+	// }
+	// var x = [5]float64{
+	// 	1,
+	// 	2,
+	// 	3,
+	// 	4,
+	// 	5,
+	// }
+	// var y = []float64
+	// x[4] = 100
+	// x[1] = 12
+	// var total float64 = 0
+	// for _, value := range x {
+	// 	total += value
+	// }
+	// fmt.Println(total / float64(len(x)))
+	// i := 0
+	// switch i {
+	// case 0:
+	// 	fmt.Println("Zero")
+	// case 1:
+	// 	fmt.Println("One")
+	// case 2:
+	// 	fmt.Println("Two")
+	// case 3:
+	// 	fmt.Println("Three")
+	// case 4:
+	// 	fmt.Println("Four")
+	// case 5:
+	// 	fmt.Println("Five")
+	// default:
+	// 	fmt.Println("Unknown Number")
+	// }
+	// x := []float64{1, 2, 3, 4, 5, 6}
+	// fmt.Println(x)
+	// slice1 := []int{1, 2, 3}
+	// slice2 := make([]int, 2)
+	// copy(slice2, slice1)
+	// fmt.Println(slice2)
+	x := []int{
+		48, 96, 86, 68,
+		57, 82, 63, 70,
+		37, 34, 83, 27,
+		19, 97, 9, 17,
+	}
+
+	fmt.Println(x)
+}
+
+// hello world
---
+++ 1_type.go
@@ -0,0 +1,10 @@
+package main
+
+
+import (
+	"fmt"
+)
+
+func myPrint()
+	fmt.Println("222")
+}
